6cc7f469e090e726b21674f9393aab7739e4b96f
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-12 20:30:31 -0500

    commit tree 5dadeaea4f594a7b7966aa77d4e75f5c7201710c

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,51 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(given_name)
+    song = self.new
+    song.name = given_name
+    song
+  end
+
+  def self.create_by_name(given_name)
+    song = self.new_by_name(given_name)
+    song.save
+    song
+  end
+
+  def self.new_from_filename(filename)
+    info = filename.split(" - ")
+    info[1].gsub!(".mp3","")
+    song = self.new
+    song.name = info[1]
+    song.artist_name = info[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else
+      return self.create_by_name(name)
+    end
+  end
+
   def self.all
     @@all
   end
@@ -9,5 +54,13 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.alphabetical
+    names = self.all.sort_by{|song| song.name}
+  end
+
+  def self.destroy_all
+    @@all = []
+  end  
 
 end
